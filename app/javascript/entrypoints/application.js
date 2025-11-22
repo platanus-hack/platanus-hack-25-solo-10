36cab5a0205d7cf4a4a676cdfd64c8304a6b86ad
ivanwolf <ivanwolf15@gmail.com>
2025-11-22 15:29:13 -0300

    install turbo

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,28 +1,23 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+// Import Turbo for Hotwire
+import '@hotwired/turbo-rails'
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+// Import and start Stimulus
+import { Application } from '@hotwired/stimulus'
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+const application = Application.start()
+application.debug = false
+window.Stimulus = application
 
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+// Auto-load all Stimulus controllers
+const controllers = import.meta.glob('../controllers/**/*_controller.js', { eager: true })
+Object.entries(controllers).forEach(([path, module]) => {
+  const controllerName = path
+    .replace('../controllers/', '')
+    .replace('_controller.js', '')
+    .replace(/\//g, '--')
+    .replace(/_/g, '-')
+  
+  if (module.default) {
+    application.register(controllerName, module.default)
+  }
+})
