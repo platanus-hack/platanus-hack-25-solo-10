0caf46e3a96212f3bb1d37327fd911b6203a8ee7
ivanwolf <ivanwolf15@gmail.com>
2025-11-23 02:45:07 -0300

    add controller to scroll

---
+++ app/javascript/controllers/auto_scroll_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.scrollToBottom()
+    this.observer = new MutationObserver(() => {
+      this.scrollToBottom()
+    })
+
+    this.observer.observe(this.element, {
+      childList: true,
+      subtree: true
+    })
+  }
+
+  disconnect() {
+    if (this.observer) {
+      this.observer.disconnect()
+    }
+  }
+
+  scrollToBottom() {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+    })
+  }
+}
+
